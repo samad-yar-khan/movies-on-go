ea253c067b5e80de5f2fd86d055ac9ec333ec475
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-04-16 02:17:07 +0530

    Setup project

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {data} from '../data'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+        <div className='list'>
+          {data.map(movie => {
+            <MovieCard movie={movie} />
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

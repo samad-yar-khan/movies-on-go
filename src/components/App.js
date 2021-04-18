3891466bca0abb593b0d9c91e7885257d6ebbb73
samad-yar-khan <70485812+samad-yar-khan@users.noreply.github.com>
2021-04-19 03:50:39 +0530

    Change the state from array to an object containing a list array and a favouritees array

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,7 @@ class  App extends React.Component {
 
   render() {
 
-    const movies = this.props.store.getState();
+    const {list} = this.props.store.getState();
     // console.log("Render");
 
       return (
@@ -40,7 +40,7 @@ class  App extends React.Component {
               <div className="tab">Favourites</div>
             </div>
             <div className='list'>
-              {movies.map((movie , index) => {
+              {list.map((movie , index) => {
                 return <MovieCard 
                           movie={movie}
                           key={`movies-${index}`}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,24 @@
 
-import {ADD_MOVIES} from '../actions/index'
+import {ADD_MOVIES} from '../actions/index' //variables for action types 
 
-export default function movies(state =  [] , action){
+//initial state was an arra but we need to serparte the normal movie lista from the fav ones
+//so it was better to make our state be an object with 2 arrays
+const initialMovieState = {
+    list : [],
+    favourites : []
+}
+
+export default function movies(state = initialMovieState , action){
     //reducers  are passed to the action at the time f creation and hence the last return statemets sets the initial state of the store
     //afte that thhe coscetive states are set by dipattching the actions 
     //---
     //string comparisons take more time compares to var comparisons and variables can be cchanges
     //this gives us the adbility to changes name for action typpes
     if(action.type === ADD_MOVIES){
-        return action.movies;
+        return {
+            ...state ,
+            list : action.movies
+        };//new state
     }
     // console.log("heya");
     return state;
